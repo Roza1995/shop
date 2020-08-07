e783737ada393f299920c188e81b1848178d4387
Roza <rozik-petrosyan@mail.ru>
2020-08-07 20:28:21 +0400

    input and output

--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Product } from './../core/models/product';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-products-list',
@@ -6,10 +7,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products-list.component.scss']
 })
 export class ProductsListComponent implements OnInit {
+  public products : Product[] = [];
+  public selectedProduct: Product;
 
-  constructor() { }
+  constructor() {
+    this.products = [
+      new Product(
+      1, 
+      "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
+      "Shoes",
+      100,
+      "Lightweight construction with breathable mesh fabric for maximum comfort and performance.Lace-up closure for a snug fit.High quality EVA sole for traction and exceptional durability."
+      ),
+      new Product(
+        2, 
+        "https://images.pexels.com/photos/1598505/pexels-photo-1598505.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        "Shoes",
+        150,
+        "Lightweight construction with breathable mesh fabric for maximum comfort and performance.Lace-up closure for a snug fit.High quality EVA sole for traction and exceptional durability."
+        ),
+      new Product(
+          3, 
+          "https://images.pexels.com/photos/2529148/pexels-photo-2529148.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+          "Shoes",
+          140,
+          "Lightweight construction with breathable mesh fabric for maximum comfort and performance.Lace-up closure for a snug fit.High quality EVA sole for traction and exceptional durability."
+          )
+    
+    ]
+   }
 
   ngOnInit(): void {
+  }
+
+  getSelected(product: Product): void{
+    this.selectedProduct = product;
+  }
+
+
+  public isSelected(product: Product): boolean {
+    if(product && this.selectedProduct && product.id === this.selectedProduct.id){
+      return true;
+    }
+      return false;
   }
 
 }
