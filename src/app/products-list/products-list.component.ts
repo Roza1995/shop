5b311ea1d56526ea33c4e09a25141a1b8a477e7b
Roza <rozik-petrosyan@mail.ru>
2020-08-15 17:07:28 +0400

    service and http

--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -1,13 +1,16 @@
 import { Product } from './../core/models/product';
 import { Component, OnInit, ContentChild, ElementRef} from '@angular/core';
 import { NgForm} from '@angular/forms';
+import { DataService } from './../core/services/data.service';
 
 @Component({
   selector: 'app-products-list',
   templateUrl: './products-list.component.html',
-  styleUrls: ['./products-list.component.scss']
+  styleUrls: ['./products-list.component.scss'],
+  providers: [ DataService ]
 })
 export class ProductsListComponent implements OnInit {
+
   @ContentChild("contentChild", {static:false})
   public contentChild: ElementRef;
   public products : Product[] = [];
@@ -19,43 +22,28 @@ export class ProductsListComponent implements OnInit {
   public product: string[] = ['price','price-reverse','alphabetical','alphabetical-reverse'];
   public isLoading: boolean = true;
   public modifiedText:string;
-  
+
+
 
   
 
-  constructor() {
+  constructor(private dataservice: DataService) {
     
    }
 
   ngOnInit(): void {
     setTimeout(this._getProducts.bind(this),1000);
+
+   /*  this.dataservice.getTodos()
+    .subscribe((res)=> {
+      console.log(res);
+    }, (err)=>{
+      console.log(err);
+    }) */
   }
 
   private _getProducts(): void{
-    this.cachedProducts = this.products = [
-      new Product(
-      1, 
-      "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
-      "Red Nike",
-      300,
-      "Lightweight construction with breathable mesh fabric for maximum comfort and performance.Lace-up closure for a snug fit.High quality EVA sole for traction and exceptional durability.",
-      ),
-      new Product(
-        2, 
-        "https://images.pexels.com/photos/1598505/pexels-photo-1598505.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-        "Grey Nike",
-        150,
-        "Lightweight construction with breathable mesh fabric for maximum comfort and performance.Lace-up closure for a snug fit.High quality EVA sole for traction and exceptional durability.",
-        ),
-      new Product(
-          3, 
-          "https://images.pexels.com/photos/2529148/pexels-photo-2529148.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-          "Puma",
-          190,
-          "Lightweight construction with breathable mesh fabric for maximum comfort and performance.Lace-up closure for a snug fit.High quality EVA sole for traction and exceptional durability.",
-          )
-    
-    ]
+    this.dataservice.getProductsFromDB().subscribe(data => this.products = data["products"]);
     this.isLoading = !this.isLoading;
   }
 
@@ -90,19 +78,19 @@ export class ProductsListComponent implements OnInit {
     setTimeout(()=>{
       switch(product){
         case "price":
-          this.products = this.cachedProducts.sort(this.SortByPrice);
+          this.products = this.cachedProducts.sort(this.sortByPrice);
           console.log(this.products);
           break;
         case "price-reverse":
-          this.products = this.cachedProducts.sort(this.SortByPrice).reverse();
+          this.products = this.cachedProducts.sort(this.sortByPrice).reverse();
           console.log(this.products);
           break;
         case  "alphabetical":
-          this.products = this.cachedProducts.sort(this.SortByTitle);
+          this.products = this.cachedProducts.sort(this.sortByTitle);
           console.log(this.products);
           break;
         case "alphabetical-reverse":
-          this.products = this.cachedProducts.sort(this.SortByTitle).reverse();
+          this.products = this.cachedProducts.sort(this.sortByTitle).reverse();
           console.log(this.products);
           break;
       }
@@ -112,13 +100,13 @@ export class ProductsListComponent implements OnInit {
     
   }
 
-  public SortByPrice(price1: Product, price2: Product): number{
+  public sortByPrice(price1: Product, price2: Product): number{
     if(price1.price > price2.price)return 1
      else if(price1.price ===  price2.price) return 0
       else return -1
   }
 
-  public SortByTitle(title1: Product, title2: Product): number{
+  public sortByTitle(title1: Product, title2: Product): number{
     if(title1.title > title2.title)return 1
     else if(title1.title ===  title2.title) return 0
      else return -1
